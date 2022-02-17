8d87759015cab55165125e359034d584c7e374d0
eggplantzzz <tmfeickert@gmail.com>
2022-02-17 12:46:29 -0500

    begin implementing web3 stuff

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import { Component } from "react";
 import './App.css';
 import PetList from "./PetList";
 import pets from "./pets";
-// import Adoption from "./build/contracts/Adoption";
+import Adoption from "./build/contracts/Adoption";
+
+// let app know that about globals loaded in the html
+/*global TruffleContract*/
+/*global Web3*/
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       contracts: {},
-      web3Provider: null
+      web3Provider: null,
+      web3: null
     };
   }
 
@@ -20,19 +25,20 @@ class App extends Component {
 
   async initContract() {
     console.log("initContract");
-    // const AdoptionArtifact = TruffleContract(Adoption);
+    const AdoptionArtifact = TruffleContract(Adoption);
 
     // Set the provider for our contract
-    // AdoptionArtifact.setProvider(App.web3Provider);
-    //
-    // this.setState({
-    //   contracts: {
-    //     Adoption: AdoptionArtifact
-    //   }
-    // });
+    AdoptionArtifact.setProvider(this.web3Provider);
+
+    this.setState({
+      contracts: {
+        Adoption: AdoptionArtifact
+      }
+    });
+    console.log("the state -- %o", this.state);
 
     // Use our contract to retrieve and mark the adopted pets
-    // await this.markAdopted();
+    await this.markAdopted();
   }
 
   async markAdopted() {
@@ -40,27 +46,28 @@ class App extends Component {
   }
 
   async initWeb3() {
-    console.log("initWeb3");
     // Modern dapp browsers...
-    // if (window.ethereum) {
-    //   App.web3Provider = window.ethereum;
-    //   try {
-    //     // Request account access
-    //     await window.ethereum.enable();
-    //   } catch (error) {
-    //     // User denied account access...
-    //     console.error("User denied account access")
-    //   }
-    // }
-    // // Legacy dapp browsers...
-    // else if (window.web3) {
-    //   App.web3Provider = window.web3.currentProvider;
-    // }
-    // // If no injected web3 instance is detected, fall back to Ganache
-    // else {
-    //   App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
-    // }
-    // web3 = new Web3(App.web3Provider);
+    if (window.ethereum) {
+      this.web3Provider = window.ethereum;
+      try {
+        // Request account access
+        await window.ethereum.enable();
+      } catch (error) {
+        // User denied account access...
+        console.error("User denied account access")
+      }
+    }
+    // Legacy dapp browsers...
+    else if (window.web3) {
+      this.web3Provider = window.web3.currentProvider;
+    }
+    // If no injected web3 instance is detected, fall back to Ganache
+    else {
+      this.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+    }
+    this.setState({
+      web3: new Web3(this.web3Provider)
+    });
   }
 
   render() {
