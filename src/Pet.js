3a2c7ff1b80959707f4e8dee4aafbd9409d6a4ad
eggplantzzz <tmfeickert@gmail.com>
2022-02-17 11:51:59 -0500

    flesh out pet info and add click handler for adopt

--- src/Pet.js
+++ src/Pet.js
@@ -1,22 +1,30 @@
+import { Component } from "react";
 import "./Pet.css";
 
-function Pet(props) {
-  const { age, name, breed, location, picture } = props;
-  return (
-    <div className="Pet">
-      <div className="name">{name}</div>
-      <img src={picture} className="pet-image" alt="pet" />
-      <div className="breed">
-        <strong>Breed:</strong> {breed}
+class Pet extends Component {
+  handleAdoptClick() {
+    console.log("adopt clicked");
+  }
+
+  render() {
+    const { age, name, breed, location, picture } = this.props;
+    return (
+      <div className="Pet">
+        <div className="name">{name}</div>
+        <img src={picture} className="pet-image" alt="pet" />
+        <div className="breed">
+          <strong>Breed:</strong> {breed}
+        </div>
+        <div className="age">
+          <strong>Age:</strong> {age}
+        </div>
+        <div className="location">
+          <strong>Location:</strong> {location}
+        </div>
+        <div onClick={this.handleAdoptClick} className="adopt-button">adopt</div>
       </div>
-      <div className="age">
-        <strong>Age:</strong> {age}
-      </div>
-      <div className="location">
-        <strong>Location:</strong> {location}
-      </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default Pet;
