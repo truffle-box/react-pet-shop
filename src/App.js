02cefd987c726cad7c13c0f4223f6b6dc0b15a72
eggplantzzz <tmfeickert@gmail.com>
2022-02-18 12:36:37 -0500

    remove web3 and finish off adoption flow for pets

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { Component } from "react";
 import './App.css';
-import PetList from "./PetList";
+import Pet from "./Pet";
 import pets from "./pets";
 import Adoption from "./build/contracts/Adoption";
+import detectEthereumProvider from "@metamask/detect-provider";
 
 // let app know that about globals loaded in the html
 /*global TruffleContract*/
-/*global Web3*/
 
 class App extends Component {
   constructor(props) {
@@ -18,13 +18,12 @@ class App extends Component {
     this.state = {
       adopters,
       contracts: {},
-      web3Provider: null,
-      web3: null
+      provider: null,
     };
   }
 
   async componentDidMount() {
-    await this.initWeb3();
+    await this.initProvider();
     await this.initContract();
   }
 
@@ -32,7 +31,7 @@ class App extends Component {
     const AdoptionArtifact = TruffleContract(Adoption);
 
     // Set the provider for our contract
-    AdoptionArtifact.setProvider(this.state.web3Provider);
+    AdoptionArtifact.setProvider(this.state.provider);
 
     this.setState({
       contracts: {
@@ -48,51 +47,28 @@ class App extends Component {
     const adoptionInstance = await this.state.contracts.Adoption.deployed();
     const adopters = await adoptionInstance.getAdopters();
     this.setState({ adopters });
-    console.log("marking adopted -- %o", this.state.adopters);
   }
 
   async handleAdopt(petId) {
-    console.log("the state -- %o", this.state);
     const adoptionInstance = await this.state.contracts.Adoption.deployed();
-    const accounts = await this.state.web3.eth.getAccounts();
-    console.log("the accounts -- %o", petId);
+    const accounts = await this.state.provider.request({
+      method: "eth_accounts"
+    });
     // use the first address as the adopter
-    const result = await adoptionInstance.adopt(petId, { from: accounts[0] });
+    await adoptionInstance.adopt(petId, { from: accounts[0] });
     await this.markAdopted();
   }
 
-  async initWeb3() {
-    // Modern dapp browsers...
-    if (window.ethereum) {
+  async initProvider() {
+    const provider = await detectEthereumProvider();
+    if (provider) {
       this.setState({
-        web3Provider: window.ethereum
+        provider
       });
-      try {
-        // Request account access
-        await window.ethereum.enable();
-      } catch (error) {
-        // User denied account access...
-        console.error("User denied account access")
-      }
+      const accounts = await this.state.provider.request({ method: "eth_accounts" });
+    } else {
+      console.log("please install mm");
     }
-    // Legacy dapp browsers...
-    else if (window.web3) {
-      this.setState({
-        web3Provider: window.web3.currentProvider
-      });
-    }
-    // If no injected web3 instance is detected, fall back to Ganache
-    else {
-      this.setState({
-        web3Provider: new Web3.providers.HttpProvider("http://localhost:8545")
-      });
-    }
-    console.log("this.web3Provider - %o", this.state.web3Provider);
-    this.setState({
-      web3: new Web3(this.state.web3Provider)
-    });
-    const accounts = this.state.web3.eth.getAccounts();
-    console.log("the acc -- %o", accounts);
   }
 
   render() {
@@ -102,7 +78,9 @@ class App extends Component {
           <p>
             Pete's Pet Shop
           </p>
-          <PetList pets={pets} adopters={this.state.adopters} handleAdopt={this.handleAdopt.bind(this)} />
+          <div className="PetList">
+            {pets.map((pet, index) => <Pet {...pet} handleAdopt={this.handleAdopt.bind(this, pet.id)} adopter={this.state.adopters[pet.id]} key={index} />)}
+          </div>
         </header>
       </div>
     );
--- src/Pet.js
+++ src/Pet.js
@@ -2,15 +2,19 @@ import { Component } from "react";
 import "./Pet.css";
 
 class Pet extends Component {
-  async handleAdoptClick() {
-    console.log("the props -- %o", this.props);
-    await this.props.handleAdopt();
-    console.log("adopt clicked");
+  adoptButton() {
+    const adopted = this.props.adopter !== "0x0000000000000000000000000000000000000000";
+    return adopted ?
+      <div className="inactive-adopt-button">
+        adopted
+      </div> :
+      <div onClick={this.props.handleAdopt} className="adopt-button">
+        adopt
+      </div>
   }
 
   render() {
-    const { age, name, breed, location, picture, adopter, id } = this.props;
-    const adopted = adopter !== "0x0000000000000000000000000000000000000000";
+    const { age, name, breed, location, picture, id } = this.props;
     return (
       <div className="Pet">
         <div className="name">{name}</div>
@@ -24,9 +28,7 @@ class Pet extends Component {
         <div className="location">
           <strong>Location:</strong> {location}
         </div>
-        <div onClick={this.handleAdoptClick.bind(this, id)} className="adopt-button">
-          { adopted ? "already adopted" : "adopt" }
-        </div>
+        {this.adoptButton()}
       </div>
     );
   }
