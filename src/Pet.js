c8dd0de3149f7f2742ad69e327114ffdd9ed76d5
eggplantzzz <tmfeickert@gmail.com>
2022-02-24 16:22:57 -0500

    make Pet a functional component

--- src/Pet.js
+++ src/Pet.js
@@ -1,37 +1,34 @@
-import { Component } from "react";
 import "./Pet.css";
 
-class Pet extends Component {
-  adoptButton() {
-    const adopted = this.props.adopter !== "0x0000000000000000000000000000000000000000";
+function Pet (props) {
+  const adoptButton = () => {
+    const adopted = props.adopter !== "0x0000000000000000000000000000000000000000";
     return adopted ?
       <div className="inactive-adopt-button">
         adopted
       </div> :
-      <div onClick={this.props.handleAdopt} className="adopt-button">
+      <div onClick={props.handleAdopt} className="adopt-button">
         adopt
       </div>
   }
 
-  render() {
-    const { age, name, breed, location, picture, id } = this.props;
-    return (
-      <div className="Pet">
-        <div className="name">{name}</div>
-        <img src={picture} className="pet-image" alt="pet" />
-        <div className="breed">
-          <strong>Breed:</strong> {breed}
-        </div>
-        <div className="age">
-          <strong>Age:</strong> {age}
-        </div>
-        <div className="location">
-          <strong>Location:</strong> {location}
-        </div>
-        {this.adoptButton()}
+  const { age, name, breed, location, picture, id } = props;
+  return (
+    <div className="Pet">
+      <div className="name">{name}</div>
+      <img src={picture} className="pet-image" alt="pet" />
+      <div className="breed">
+        <strong>Breed:</strong> {breed}
       </div>
-    );
-  }
+      <div className="age">
+        <strong>Age:</strong> {age}
+      </div>
+      <div className="location">
+        <strong>Location:</strong> {location}
+      </div>
+      {adoptButton()}
+    </div>
+  );
 };
 
 export default Pet;
